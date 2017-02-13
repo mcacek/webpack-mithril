28a7c5ea5e594f3c8694f60be1bee57430797f6e
Michael Cacek <michael.cacek@gmail.com>
2017-02-12 19:35:04 -0600

    Initial webpack configuration

---
+++ config/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path')
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+	entry: {
+		app: './src/js/app.js',
+		vendor: 'mithril'
+	},
+	module: {
+		rules: [{
+			test: /\.js$/,
+			exclude: /node_modules/,
+			loader: 'babel-loader'
+		},
+		{
+			test: /\.css$/,
+			loader: ExtractTextPlugin.extract({
+				fallback: 'style-loader',
+				use: [{
+					loader: 'css-loader',
+					options: {
+						importLoaders: 2
+					}
+				}, {
+					loader: 'postcss-loader'
+				}]
+			})
+		}
+		]
+	},
+	plugins: [
+		new ExtractTextPlugin('main.css'),
+		new webpack.optimize.CommonsChunkPlugin({
+			names: ['vendor', 'manifest']
+		})
+	],
+	output: {
+		filename: '[chunkhash].[name].js',
+		path: path.resolve(__dirname, '../dist')
+	}
+}
---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+import m from 'mithril'
+import '../css/main.css'
+
+var root = document.getElementById('root')
+
+m.render(root, m('h1', 'My first page!'))
